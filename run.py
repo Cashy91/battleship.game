aa492849ace7d7103c105c8108bf923a7f3cddfa
Joakim Karlsson <jocke19_91@hotmail.com>
2023-03-04 10:15:45 +0000

    add letter conversion to numbers

--- run.py
+++ run.py
@@ -4,3 +4,5 @@ from random import randint
 hidden_board = [[" " for _ in range(8)] for _ in range(8)]
 guess_board = [[" " for _ in range(8)] for _ in range(8)]
 
+# Converts letter to numbers
+to_num = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
