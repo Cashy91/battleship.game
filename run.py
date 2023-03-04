d624d287510c25a5e0bccc21c65d8a026cccfac8
Joakim Karlsson <jocke19_91@hotmail.com>
2023-03-04 10:18:20 +0000

    add info for user at start of game

--- run.py
+++ run.py
@@ -6,3 +6,10 @@ guess_board = [[" " for _ in range(8)] for _ in range(8)]
 
 # Converts letter to numbers
 to_num = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
+
+# Game info for user understanding
+print("-----------------------------")
+print("You have entered the\nbattlefield take aim and fire".upper())
+print("-----------------------------")
+print("You have 15 shots to take\ndown the 5 battleships".upper())
+print("-----------------------------")
